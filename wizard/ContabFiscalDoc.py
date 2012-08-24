75b522bf008ed3b9993c6c5db4c3763d96b3c3ec
giuseppe <g.dalo@cgsoftware.it>
2012-08-24 13:09:09 +0200

    Contabilizzazione migliorata sulle righe negative

--- wizard/ContabFiscalDoc.py
+++ wizard/ContabFiscalDoc.py
@@ -140,8 +140,9 @@ class contab_fiscaldoc(osv.osv_memory):
             if scritto[1]:
                #import pdb;pdb.set_trace()
                notok= move_obj.button_validate(cr,uid,[id_reg],context)
+               ok = self.pool.get('fiscaldoc.header').write(cr,uid,[doc.id],{'registrazione':id_reg})
                if notok==False:
-                    ok = self.pool.get('fiscaldoc.header').write(cr,uid,[doc.id],{'registrazione':id_reg})              
+                                  
                     scritto[0] = scritto[0]+ "  Incasso Documento "+ doc.name + "  CONTABILIZZATO ALLA REGISTRAZIONE "+ move_head.name +'\n'
                else:
                      #import pdb;pdb.set_trace()
@@ -333,7 +334,7 @@ class contab_fiscaldoc(osv.osv_memory):
                      else:
                         segno = "AV"       
                      if segno=="DA":
-                        if netto>0:
+                        if  True :# netto>0:  messo in rem per sommare algebricamnete sul totale conto se c'è solo l'importo negativo
                         #segno dare
                             riga['credit']+=0
                             riga['debit']+=netto   
@@ -343,7 +344,7 @@ class contab_fiscaldoc(osv.osv_memory):
  
                      else:
                         #segno dare
-                        if netto>0:
+                        if True : #netto>0: messo in rem per sommare algebricamnete sul totale conto si incazzerà se c'è solo l'importo negativo
                             riga['credit']+=netto     
                             riga['debit']+=0
                         else:
